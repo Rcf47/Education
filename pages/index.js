6a22edf72bf1c199f0a65d72cd2a62f51bef6a23
Vadim Cherepanov <xenos26@gmail.com>
2023-03-08 00:15:23 +0700

    Heading and Boxes in index.js and app.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,22 @@
+import { Container, Box, Heading } from "@chakra-ui/react"
+
 const Page = () => {
-  return <div>Hello</div>
+  return (
+    <Container>
+      <Box borderRadius="lg" bg="red" p={3} mb={6} align="center">
+        Hello, I&apos;m a developer.
+      </Box>
+      <Box display={{ md: 'flex' }}>
+        <Box flexGrow={1}>
+          <Heading as="h2" variant="page-title">
+            Vadim Cherepanov
+          </Heading>
+          <p>We love what we do</p>
+        </Box>
+      </Box>
+    </Container>
+  )
 }
+
 
 export default Page
