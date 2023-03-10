a340ee879c4f48f1f9e27d453babf11c5f8814b8
Vadim Cherepanov <xenos26@gmail.com>
2023-03-10 23:18:54 +0700

    add Image and Section

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,25 @@
-import { Container, Box, Heading } from "@chakra-ui/react"
+import NextLink from 'next/link'
+import {
+  Container,
+  Box,
+  Heading,
+  Image,
+  useColorModeValue,
+  Link
+} from '@chakra-ui/react'
+import Section from '../components/section'
+import Paragraph from '../components/paragraph'
 
 const Page = () => {
   return (
     <Container>
-      <Box borderRadius="lg" bg="red" p={3} mb={6} align="center">
+      <Box
+        borderRadius="lg"
+        bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+        p={3}
+        mb={6}
+        align="center"
+      >
         Hello, I&apos;m a developer.
       </Box>
       <Box display={{ md: 'flex' }}>
@@ -13,10 +29,37 @@ const Page = () => {
           </Heading>
           <p>We love what we do</p>
         </Box>
+        <Box
+          flexShrink={0}
+          mt={{ base: 4, md: 0 }}
+          ml={{ md: 6 }}
+          align="center"
+        >
+          <Image
+            borderColor="whiteAlpha.800"
+            borderWidth={2}
+            borderStyle="solid"
+            maxWidth="100px"
+            display="inline-block"
+            borderRadius="full"
+            src="/images/vadim-min.png"
+            alt="Profile Image"
+          />
+        </Box>
       </Box>
+      <Section delay={0.1}>
+        <Heading as="h3" variant="section-title">
+          Work
+        </Heading>
+        <Paragraph>
+          Dev as lifestyle{' '}
+          <NextLink href="/works/inkdrop">
+            <Link>Inkdrop</Link>
+          </NextLink>
+        </Paragraph>
+      </Section>
     </Container>
   )
 }
-
 
 export default Page
